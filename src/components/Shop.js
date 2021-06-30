d1517a48a0a9be41f2dcd91f7f251accbd548bbf
Alexander Sherwin <alx.sherwin@gmail.com>
2021-06-30 11:54:55 -0600

    first commit

---
+++ src/components/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function Shop(){
+    return(
+        <main>
+            <h1> Give me your fucking MONEY!!</h1>
+        </main>
+    )
+}
+
+export default Shop;
